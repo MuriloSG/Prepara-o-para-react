ebb9f68284bb4041b48969b9a71c306501a7be84
MuriloSG <muriloprojetosti@gmail.com>
2022-12-21 15:12:30 -0300

    :sparkles: finish the BoardGame obeject

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,14 +1,11 @@
 import "./style.css";
 
 function CardGame() {
-    return `
-        <article class = "card-game"> 
-            <img src ="src/images/alura-pixel.png" alt = "logo alura">
-
-        <\article>
-    
-    `;
-
+  return `
+    <article class="card-game">
+      <img src="src/images/alura-pixel.png" alt="Logo da Alura">
+    </article>
+  `;
 }
 
 export default CardGame;
