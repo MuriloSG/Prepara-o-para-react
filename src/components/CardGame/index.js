0007da049a0607583333c94c550afab7b3f5a8f8
MuriloSG <muriloprojetosti@gmail.com>
2022-12-20 22:24:39 -0300

    :tada: create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function CardGame() {
+    return `
+        <article class = "card-game"> 
+            <img src ="src/images/alura-pixel.png" alt = "logo alura">
+
+        <\article>
+    
+    `;
+
+}
+
+export default CardGame;
