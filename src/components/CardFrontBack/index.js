09dc9898f2a294dd35a496ff779d589fe519aa21
MuriloSG <muriloprojetosti@gmail.com>
2022-12-22 11:48:01 -0300

    :sparkles: create function event in cardFrontBack component

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,9 +1,21 @@
-import "./style.css"
+import "./style.css";
 import CardGame from "../CardGame";
+
+window.cardFrontBack = {};
+window.cardFrontBack.handClick = (event) => {
+    const $origin = event.target;
+    const $cardFrontBack = $origin.closest(`.card-front-back`);
+    
+    if($cardFrontBack.classList.contains(`-active`)){
+        $cardFrontBack.classList.remove(`-active`);
+    }else{
+        $cardFrontBack.classList.add(`-active`);
+    }
+}
 
 function CardFrontBack(){
     return` 
-        <article class = "card-front-back"> 
+        <article class = "card-front-back" onClick="cardFrontBack.handClick(event)"> 
             <div class = "card -front">
                 ${CardGame()}
             </div>
